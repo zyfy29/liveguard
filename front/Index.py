8938cafaaccccaa3dd10ad49bfec05960a724ac9
zyfy <wasuremono127@gmail.com>
2025-02-04 15:07:03 +0900

    Refactor task page and improve task restoration logic
    Reorganized code by renaming and restructuring files for clarity. Enhanced the task restoration functionality with improved logic and UI updates. Simplified navigation logic and removed unused code to streamline the application.

---
+++ front/Index.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title='Liveguard Panel', layout='wide')
+st.sidebar.success("Select a page above.")
+st.title("Hello!")
--- front/pages/task.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-from front.api_client import get_tasks
-
-
-def task_page():
-    st.title('Summarize Tasks')
-    tasks = get_tasks()
-    edited_task_dicts = st.data_editor(tasks)
